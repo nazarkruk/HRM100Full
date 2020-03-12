5833f427ed9dccab259d4b5cb3c3881d1b60ffb2
nazarkruk <nazarkruk@icloud.com>
2020-03-12 14:08:38 -0400

    login test fixed

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,21 +1,17 @@
 import unittest
-from time import sleep
 from selenium import webdriver
-
-from fixtures.base import BaseTestCase
 from fixtures.params import CHROME_EXECUTABLE_PATH, DOMAIN
 from pages.login_page import LoginPage
-from pages.personal_details_page import PersonalDetailsPage
 
 
-class MyLoginCase(unittest.TestCase):
+class LoginTestCase(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path=CHROME_EXECUTABLE_PATH)
         self.driver.get(DOMAIN)
         self.login_page = LoginPage(self.driver)
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.driver.quit()
 
     def test_01_valid_login(self):
@@ -59,7 +55,7 @@ class MyLoginCase(unittest.TestCase):
         self.login_page.press_login_button()
 
         spam_massage = driver.find_element_by_id('spanMessage').text
-        self.assertEqual ('Username cannot be empty', spam_massage)
+        self.assertEqual('Username cannot be empty', spam_massage)
 
 
 if __name__ == '__main__':
